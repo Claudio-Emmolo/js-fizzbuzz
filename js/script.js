6e69b06eb53a51e1111a6d32c74f24ae7ca1bcfe
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-23 15:56:45 +0100

    Print number on html

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 /**
  * Creare un For per generare i numeri da 1 a 100;
- * Stampare i numeri nella console
  * Creare un IF dove i numeri multipli di 3 si chiamano "Fizz",
  * di 5 si chiamano "Buzz",
  * di 3 e di 5 si chiamano "FizzBuzz"
+ * Stampare i restanti numeri
  * 
  * --BONUS--
  * Successivamente stampare tutti i numeri nella section#number-list;
  * Aggiunge carateristiche CSS diverse ad ogni Fizz, Buzz, FizzBuzz o numero...
  */
+const printNumberList = document.getElementById("number-list")
 
 for (let i = 1; i <= 100; i++){
+
     
     if ((i % 3 == 0) && (i % 5 == 0)){
-        console.log( i + " FizzBuzz");
+        printNumberList.innerHTML += `<h1>FizzBuzz</h1>`;
     } else if (i % 5 == 0){
-        console.log(i + " Buzz");
+        printNumberList.innerHTML += `<h1>Buzz</h1>`;
     } else if (i % 3 == 0){
-        console.log(i + " Fizz");
+        printNumberList.innerHTML += `<h1>Fizz</h1>`;
     } else{
-        console.log (i)
+        printNumberList.innerHTML += `<h1>${i}</h1>`;
     }
 }
