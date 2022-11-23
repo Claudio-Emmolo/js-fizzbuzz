96c7a47b496571fa9da09f2a3c222b26baf718f1
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-23 14:51:42 +0100

    Algorithm description

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Creare un For per generare i numeri da 1 a 100;
+ * Stampare i numeri nella console
+ * Creare un IF dove i numeri multipli di 3 si chiamano "Fizz",
+ * di 5 si chiamano "Buzz",
+ * di 3 e di 5 si chiamano "FizzBuzz"
+ * 
+ * --BONUS--
+ * Successivamente stampare tutti i numeri nella section#number-list;
+ * Aggiunge carateristiche CSS diverse ad ogni Fizz, Buzz, FizzBuzz o numero...
+ */
