41eb53b865ca3dae29b1639fcff852c3ace8f58e
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-23 15:44:40 +0100

    Generated FizzBuzz, multiple of 3 and 5

--- js/script.js
+++ js/script.js
@@ -11,11 +11,14 @@
  */
 
 for (let i = 1; i <= 100; i++){
-    console.log(i);
     
-    if (i % 3 == 0){
-        console.log("Fizz")
+    if ((i % 3 == 0) && (i % 5 == 0)){
+        console.log( i + " FizzBuzz");
     } else if (i % 5 == 0){
-        console.log("Buzz")
+        console.log(i + " Buzz");
+    } else if (i % 3 == 0){
+        console.log(i + " Fizz");
+    } else{
+        console.log (i)
     }
 }
